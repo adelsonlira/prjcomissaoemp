1f729bf887e8832caa45094dd0e575809bd89fa3
Adelson Oliveira <adelsonlira@yahoo.com.br>
2022-01-21 01:50:42 -0300

    Update ModuloConexao.java

--- src/br/com/comissaoemp/dal/ModuloConexao.java
+++ src/br/com/comissaoemp/dal/ModuloConexao.java
@@ -20,7 +20,7 @@ public class ModuloConexao {
         java.sql.Connection conexao = null;
         String driver = "com.mysql.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/dbcomissoes";
-        String user = "root";
+        String user = "";
         String password = "";
         
         try {
